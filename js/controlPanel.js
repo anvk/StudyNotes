700fa2b561736b713d8b34658a9bedbae96a7ae5
Yura Zenevich <yura.zenevich@gmail.com>
2012-03-24 09:14:33 -0400

    Added some inital styling to control panel

--- js/controlPanel.js
+++ js/controlPanel.js
@@ -12,11 +12,15 @@
 
     var setupStudyNotes = function (that) {
         that.panel = studyNotes.controlPanel.panel(that.panelContainer);
+        that.latch = studyNotes.controlPanel.latch(that.latchContainer);
     };
 
     var buildMarkup = function (that) {
+        that.container.addClass("hidden");
         that.panelContainer = $("<div/>").addClass("controlPanel-panel");
+        that.latchContainer = $("<div/>").addClass("controlPanel-latch");
         that.container.append(that.panelContainer);
+        that.container.prepend(that.latchContainer);
     };
 
     studyNotes.createWidget = function (container) {
@@ -28,9 +32,6 @@
     studyNotes.controlPanel = function (container, config) {
         var that = studyNotes.createWidget(container);
 
-        that.show = function () {};
-        that.hide = function () {};
-
         buildMarkup(that);
         setupStudyNotes(that);
 
@@ -38,6 +39,11 @@
     };
 
     studyNotes.controlPanel.panel = function (container) {
+        var that = studyNotes.createWidget(container);
+        return that;
+    };
+
+    studyNotes.controlPanel.latch = function (container) {
         var that = studyNotes.createWidget(container);
         return that;
     };
